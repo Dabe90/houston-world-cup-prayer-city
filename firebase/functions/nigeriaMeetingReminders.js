668e58888c0bd45bf25c7c0500f54030c145b395
Dabe90 <abuxberkeley@gmail.com>
2026-07-11 14:54:09 -0500

    Nigeria: update Comms (Mon 8-8:30am) & Growth (Sat 8-9pm) schedules; add Group 1/3/4 as new check-in teams (Sun/Wed 9-10pm, Thu 8:30-9:30pm)

--- firebase/functions/nigeriaMeetingReminders.js
+++ firebase/functions/nigeriaMeetingReminders.js
@@ -21,9 +21,12 @@ const NIGERIA_UNITS = [
   { id: 'welcome-hospitality', label: 'Welcome & Hospitality', day: 3, start: '22:00', end: '22:30' },
   { id: 'creative', label: 'Creative Unit', day: 4, start: '20:00', end: '21:00' },
   { id: 'choir', label: 'Choir', day: 0, start: '20:30', end: '21:30' },
-  { id: 'growth-retention', label: 'Growth & Retention', day: 5, start: '20:30', end: '21:00' },
-  { id: 'communications-social', label: 'Communications & Social Media', day: 4, start: '21:30', end: '22:00' },
+  { id: 'growth-retention', label: 'Growth & Retention', day: 6, start: '20:00', end: '21:00' },
+  { id: 'communications-social', label: 'Communications & Social Media', day: 1, start: '08:00', end: '08:30' },
   { id: 'media', label: 'Media Team', day: 6, start: '19:00', end: '20:00' },
+  { id: 'group1', label: 'Group 1', day: 0, start: '21:00', end: '22:00' },
+  { id: 'group3', label: 'Group 3', day: 3, start: '21:00', end: '22:00' },
+  { id: 'group4', label: 'Group 4', day: 4, start: '20:30', end: '21:30' },
   { id: 'workers-coordinator', label: 'Workers Coordinator', day: 3, start: '21:00', end: '21:30' },
 ];
 
